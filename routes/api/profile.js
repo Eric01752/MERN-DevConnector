75fe38e25c77bd1648226642f99626b0c4a9095a
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-05-29 17:04:24 -0400

    Add profile experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,12 +36,12 @@ router.post(
     [
         auth,
         [
-        check('status', 'Status is required')
-            .not()
-            .isEmpty(),
-        check('skills', 'Skills is required')
-            .not()
-            .isEmpty()
+            check('status', 'Status is required')
+                .not()
+                .isEmpty(),
+            check('skills', 'Skills is required')
+                .not()
+                .isEmpty()
         ]
     ],
     async (req, res) => {
@@ -163,6 +163,65 @@ router.delete(
             //Delete user
             await User.findOneAndDelete({_id: req.user.id});
             res.json({msg: 'User deleted'});
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    }
+);
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+    '/experience',
+    [
+        auth,
+        [
+            check('title', 'Title is required')
+                .not()
+                .isEmpty(),
+            check('company', 'Company is required')
+                .not()
+                .isEmpty(),
+            check('from', 'From date is required')
+                .not()
+                .isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({user: req.user.id});
+
+            profile.experience.unshift(newExp);
+            await profile.save();
+
+            res.json(profile);
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
