6d0b229ff7484c72b5beafa681a85c69f44c8d7e
Eric Schmidt <eric.schmidt1350@gmail.com>
2021-06-10 16:58:04 -0400

    Display education/experience in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,13 +4,15 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 import {getCurrentProfile} from '../../actions/profile';
 
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}}) => {
 
     useEffect(() => {
         getCurrentProfile();
-    }, []);
+    }, [getCurrentProfile]);
 
     return loading && profile === null ? <Spinner /> : <Fragment>
         <h1 className="large text-primary">Dashboard</h1>
@@ -20,6 +22,8 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}
         {profile !== null ? (
         <Fragment>
             <DashboardActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
         </Fragment>
         ) : (
         <Fragment>
